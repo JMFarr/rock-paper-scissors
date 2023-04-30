c1caf69abaa7422079c0aedee00e29cc4b15c09d
Jacob Farr <17421798+JMFarr@users.noreply.github.com>
2023-04-30 16:27:31 -0700

    Create game() and add input validation to it

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ function getComputerChoice () {
 
 function formatUserInput(input){
     // Format user input so only the first letter is upper case, the rest lower case
+    if(typeof input != "string") {
+        return;
+    }
     return `${input.substr(0, 1).toUpperCase()}${input.slice(1).toLowerCase()}`;
 }
 
@@ -24,4 +27,28 @@ function playRound(playerSelection, computerSelection) {
     }
     // Default to a loss
     return `You lose. ${playerSelection} loses to ${computerSelection}.`;
+}
+
+function game() {
+    let wins = 0;
+    let losses = 0;
+    for(let i = 0; i < 5; i++) {
+        let result;
+        let userInput = formatUserInput(prompt('Choose Rock, Paper, or Scissors.'));
+        
+        while(!validateChoice(userInput)){
+            if (userInput == null ){
+                console.log("Game aborted.")
+                return;
+            }
+            console.log(userInput);
+            userInput = formatUserInput(prompt('Ivalid input. Please choose Rock, Paper, or Scissors.'));
+        }
+
+    }
+}
+
+function validateChoice(choice){
+    const validChoices = ['Rock', 'Paper', 'Scissors'];
+    return validChoices.includes(choice);
 }
