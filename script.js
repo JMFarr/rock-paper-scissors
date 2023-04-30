18c8c5ae11436717a229de49d04ea5aa9a565a1f
Jacob Farr <17421798+JMFarr@users.noreply.github.com>
2023-04-30 15:38:18 -0700

    Implement playRound()

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
 function getComputerChoice () {
     const choices = ['Rock', 'Paper', 'Scissors'];
-    return choices[Math.floor(Math.random() * 3)]
+    return choices[Math.floor(Math.random() * 3)];
 }
 
 function formatUserInput(input){
-    //format user input so only the first letter is upper case, the rest lower case
-    return `${input.substr(0, 1).toUpperCase()}${input.slice(1).toLowerCase()}`
+    // Format user input so only the first letter is upper case, the rest lower case
+    return `${input.substr(0, 1).toUpperCase()}${input.slice(1).toLowerCase()}`;
+}
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = formatUserInput(playerSelection);
+    // Check for a tie
+    if(playerSelection === computerSelection) {
+        return `It's a tie! Both choices were ${playerSelection}.`;
+    }
+    // Check for a win
+    if(
+        playerSelection === 'Rock' && computerSelection === 'Scissors'
+        || playerSelection === 'Paper' && computerSelection === 'Rock'
+        || playerSelection === 'Scissors' && computerSelection === 'Paper'
+    ) {
+        return `You win! ${playerSelection} beats ${computerSelection}.`;
+    }
+    // Default to a loss
+    return `You lose. ${playerSelection} loses to ${computerSelection}.`;
 }
