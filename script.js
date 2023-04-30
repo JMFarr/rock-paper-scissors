fa85f9485492a6cf9b8f1ab551c28ac2c7cacd40
Jacob Farr <17421798+JMFarr@users.noreply.github.com>
2023-04-30 15:21:17 -0700

    Implement formatUserInput()

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ function getComputerChoice () {
     const choices = ['Rock', 'Paper', 'Scissors'];
     return choices[Math.floor(Math.random() * 3)]
 }
+
+function formatUserInput(input){
+    //format user input so only the first letter is upper case, the rest lower case
+    return `${input.substr(0, 1).toUpperCase()}${input.slice(1).toLowerCase()}`
+}
